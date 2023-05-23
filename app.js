e9f76cc48fee8070d658178b19ab10a13fb1d472
David Pérez Numa <122552606+Davidpereznuma10@users.noreply.github.com>
2023-05-23 15:17:07 -0500

    feat: :sparkles:
    Create the App.js to call all the modules that we have created before loke the web components and the events

---
+++ app.js
@@ -0,0 +1,3 @@
+import myHeader from "./web_Componens/myheader/my-header.js";
+import myNew from   "./web_Componens/mymodal/my-modal.js"
+import myButton from "./web_Componens/mymodal/mybutton/my-button.js"; 
